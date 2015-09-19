2e76c4de1a6cd28564056eab964141b90362aeb5
Steven Orvis <steven@trappingpa.com>
2015-09-19 09:28:26 -0400

    Revert "Added vbrate button to the top of home page"
    This reverts commit dcc0a9e77840fdb5fd86f7d283a475d6c63ff392.

--- js/main.js
+++ js/main.js
@@ -94,8 +94,6 @@ var app = {
 			});
 		}
 		
-		document.addEventListener('deviceready', this.onDeviceReady, false);
-		
 		$(window).on('hashchange', $.proxy(this.route, this));
 	},
 
@@ -105,39 +103,7 @@ var app = {
 		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.store = new MemoryStore(function() {
 			self.route();
-		},
-		
-		// Bind Event Listeners
-		//
-		// Bind any events that are required on startup. Common events are:
-		// 'load', 'deviceready', 'offline', and 'online'.
-		bindEvents: function() {
-			document.addEventListener('deviceready', this.onDeviceReady, false);
-		},
-		// deviceready Event Handler
-		//
-		// The scope of 'this' is the event. In order to call the 'receivedEvent'
-		// function, we must explicity call 'app.receivedEvent(...);'
-		onDeviceReady: function() {
-			app.receivedEvent('deviceready');
-		},
-		// Update DOM on a Received Event
-		receivedEvent: function(id) {
-			var parentElement = document.getElementById(id);
-			var listeningElement = parentElement.querySelector('.listening');
-			var receivedElement = parentElement.querySelector('.received');
-
-			listeningElement.setAttribute('style', 'display:none;');
-			receivedElement.setAttribute('style', 'display:block;');
-
-			console.log('Received Event: ' + id);
-		},
-
-		vibrate: function() {
-		  navigator.notification.vibrate( 1000 );
-		}
-		
-		);
+		});
 	}
 
 };
