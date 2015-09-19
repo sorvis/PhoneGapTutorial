aefb50fae7744c299f58bcefe6653d3c55296dda
Steven Orvis <steven@trappingpa.com>
2015-09-19 09:28:04 -0400

    Revert "short ciructed to call vibrate on device ready"
    This reverts commit 6a346238c42cc8ef2e521c484cee45fd9f32c308.

--- js/main.js
+++ js/main.js
@@ -114,7 +114,6 @@ var app = {
 		// function, we must explicity call 'app.receivedEvent(...);'
 		onDeviceReady: function() {
 			app.receivedEvent('deviceready');
-			app.vibrate();
 		},
 		// Update DOM on a Received Event
 		receivedEvent: function(id) {
